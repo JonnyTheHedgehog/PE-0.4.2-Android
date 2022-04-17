a6dc5a07672005f09af4e588f22615827e2aae2a
JonnyTheHedgehog <103390574+JonnyTheHedgehog@users.noreply.github.com>
2022-04-17 16:19:39 -0300

    Hola que hace

--- source/options/CustomControlsState.hx
+++ source/options/CustomControlsState.hx
@@ -38,7 +38,7 @@ class CustomControlsState extends MusicBeatSubstate
 	var leftArrow:FlxSprite;
 	var rightArrow:FlxSprite;
 							//'hitbox',
-	var controlitems:Array<String> = ['right control', 'left control','keyboard','custom', 'hitbox'];
+	var controlitems:Array<String> = ['burned right control', 'left burned control','keyboard :)','burned custom control', 'burned hitbox '];
 
 	var curSelected:Int = 0;
 
